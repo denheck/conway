211bcaaf12b4b0eb107b73809c0586c614b512c0
Dennis Heckman <denheck@gmail.com>
2016-04-16 16:05:39 -0500

    add resurrection rules and lots of tests

--- src/conway/core.clj
+++ src/conway/core.clj
@@ -3,7 +3,6 @@
   (:require [clojure.set]))
 
 ; https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-; TODO still need cell resurrection logic
 
 (defn get-neighbor-coordinates
   "Get neighbors of the cell provided; a neighbor can be next to or diagonal to a cell"
@@ -32,8 +31,8 @@
   (let [total-living-neighbors (count (get-living-neighbors living-cell living-cells))]
     (and (> total-living-neighbors 1) (< total-living-neighbors 4))))
 
-(defn get-next-board-state
-  "Take the current board state and return the next"
+(defn get-surviving-cells
+  "Get living cells that will survive to the next generation"
   [living-cells]
   (loop [[living-cell & remaining-living-cells] living-cells
          next-generation-living-cells []]
@@ -44,16 +43,54 @@
                (conj next-generation-living-cells living-cell))
         (recur remaining-living-cells next-generation-living-cells)))))
 
-(defn start
-  "Start the simulation"
-  [initial-state]
-  (loop [ticks 0
-         living-cells initial-state]
-    (recur (get-next-board-state living-cells) (inc ticks))))
+(defn get-resurrectable-cells
+  "Takes living cells and returns all dead neighboring cells"
+  [living-cells]
+  (let [reducefn #(concat %1 (get-neighbor-coordinates %2))
+        living-cells-neighbors (distinct (reduce reducefn [] living-cells))]
+    (vec (clojure.set/difference (set living-cells-neighbors) (set living-cells)))))
 
-(defn example-board [] [[1 10] [2 9] [3 8]])
+(defn resurrect-cell?
+  "Test if a cell should come back from the dead"
+  [dead-cell living-cells]
+  (let [total-living-neghbors (count (get-living-neighbors dead-cell living-cells))]
+    (= total-living-neghbors 3)))
+
+(defn get-resurrected-cells
+  "Get cells that come back from the dead"
+  [living-cells]
+  (let [resurrectable-cells (get-resurrectable-cells living-cells)]
+    (loop [[resurrectable-cell & remaining-resurrectable-cells] resurrectable-cells
+           resurrected-cells []]
+      (if (empty? resurrectable-cell)
+        resurrected-cells
+        (if (resurrect-cell? resurrectable-cell living-cells)
+          (recur remaining-resurrectable-cells
+                 (conj resurrected-cells resurrectable-cell))
+          (recur remaining-resurrectable-cells resurrected-cells))))))
+
+(defn get-next-board-state
+  "Take the current board state and return the next"
+  [current-board-state]
+  (let [living-cells (:living-cells current-board-state)
+        ticks (:ticks current-board-state)
+        surviving-cells (get-surviving-cells living-cells)
+        resurrected-cells (get-resurrected-cells living-cells)
+        next-generation (concat surviving-cells resurrected-cells)
+        next-tick (inc ticks)]
+    {:living-cells next-generation :ticks next-tick}))
+
+(defn start-simulation
+  "Start the simulation"
+  [initial-state allowed-ticks]
+  (loop [current-board-state initial-state]
+    (if (= (:ticks current-board-state) allowed-ticks)
+      current-board-state
+      (recur (get-next-board-state current-board-state)))))
+
+(def example-state {:living-cells [[1 2] [2 2] [2 1]] :ticks 0})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (start example-board))
+  (start-simulation example-state 100))
--- test/conway/core_test.clj
+++ test/conway/core_test.clj
@@ -32,12 +32,54 @@
           is-alive? false]
       (is (= (cell-is-alive? living-cell living-cells) is-alive?)))))
 
-(deftest get-next-board-state-test
+(deftest get-surviving-cells-test
   (testing "All living cells should die from underpopulation"
     (let [living-cells [[1 2] [4 5] [6 8]]
           expected-next-generation []]
-      (is (= (get-next-board-state living-cells) expected-next-generation))))
+      (is (= (get-surviving-cells living-cells) expected-next-generation))))
   (testing "All cells should survive to the next generation"
     (let [living-cells [[1 2] [1 1] [2 1] [2 2]]
           expected-next-generation living-cells]
-      (is (= (get-next-board-state living-cells) expected-next-generation)))))
+      (is (= (get-surviving-cells living-cells) expected-next-generation)))))
+
+(deftest get-resurrectable-cells-test
+  (testing "Should return all neighbors of provided cell since their is only one living cell"
+    (let [living-cells [[1 2]]
+          expected-resurrectable-cells [[2 2] [2 3] [1 1] [1 3] [0 3] [0 2] [2 1] [0 1]]]
+      (is (= (get-resurrectable-cells living-cells) expected-resurrectable-cells))))
+  (testing "Should return only dead neighbors of 3 cells"
+    (let [living-cells [[1 2] [2 2] [2 1] [1 1]]
+          expected-resurrectable-cells [[0 0] [1 0] [2 3] [3 3] [3 0] [1 3] [0 3] [0 2] [2 0] [3 1] [3 2] [0 1]]]
+      (is (= (get-resurrectable-cells living-cells) expected-resurrectable-cells)))))
+
+(deftest resurrect-cell-test
+  (testing "Resurrect cell because it has 3 living neighbors"
+    (let [dead-cell [2 2]
+          living-cells [[1 2] [1 1] [2 1]]]
+      (is (= (resurrect-cell? dead-cell living-cells) true))))
+  (testing "Don't resurrect cell because it only has 2 living neighbors"
+    (let [dead-cell [2 2]
+          living-cells [[1 2] [1 1]]]
+      (is (= (resurrect-cell? dead-cell living-cells) false)))))
+
+(deftest get-resurrected-cells-test
+  (testing "No cells should be resurrected"
+    (let [living-cells [[1 2] [2 1]]
+          expected-resurrected-cells []]
+      (is (= (get-resurrected-cells living-cells) expected-resurrected-cells))))
+  (testing "One cell should be resurrected because it has 3 neighbors"
+    (let [living-cells [[1 2] [2 1] [1 1]]
+          expected-resurrected-cells [[2 2]]]
+      (is (= (get-resurrected-cells living-cells) expected-resurrected-cells)))))
+
+(deftest get-next-board-state-test
+  (testing "One cell should resurrect and one cell should survive, others die from underpopulation"
+    (let [living-cells [[1 2] [1 1] [2 3]]
+          board-state {:living-cells living-cells :ticks 0}
+          expected-board-state {:living-cells [[1 2] [2 2]] :ticks 1}]
+      (is (= (get-next-board-state board-state) expected-board-state))))
+  (testing "Cells don't change"
+    (let [living-cells [[1 2] [1 1] [2 1] [2 2]]
+          board-state {:living-cells living-cells :ticks 0}
+          expected-board-state {:living-cells living-cells :ticks 1}]
+      (is (= (get-next-board-state board-state) expected-board-state)))))
